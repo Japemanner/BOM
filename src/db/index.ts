396e4840adfb006760b84b8224ae50adecb6f50f
Jaap Hoeve <jaap@jaaphoeve.nl>
2026-04-15 11:04:33 +0200

    feat: voeg barrel-bestand toe en update drizzle config naar multi-schema structuur

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,10 @@
 // src/db/index.ts
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
-import * as schema from './schema'
+import * as authSchema from './schema/auth'
+import * as iamSchema from './schema/iam'
+import * as rbacSchema from './schema/rbac'
+import * as appSchema from './schema/app'
 
 const connectionString = process.env['DATABASE_URL']
 if (!connectionString) {
@@ -9,4 +12,6 @@ if (!connectionString) {
 }
 
 const client = postgres(connectionString)
-export const db = drizzle(client, { schema })
+export const db = drizzle(client, {
+  schema: { ...authSchema, ...iamSchema, ...rbacSchema, ...appSchema },
+})
---
+++ src/db/schema/index.ts
@@ -0,0 +1,6 @@
+// src/db/schema/index.ts
+// Re-exporteert alles voor backwards-compatibiliteit met bestaande imports
+export * from './auth'
+export * from './iam'
+export * from './rbac'
+export * from './app'
